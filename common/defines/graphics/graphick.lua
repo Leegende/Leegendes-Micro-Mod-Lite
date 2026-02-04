88195428bf82875cd6ad880fcbe9f92954f1a458
Fabian <71211963+Leegende@users.noreply.github.com>
2026-02-04 18:46:06 +0100

    a

--- common/defines/graphics/graphick.lua
+++ common/defines/graphics/graphick.lua
@@ -1,7 +1,11 @@
 NDefines.NGraphics.COUNTRY_COLOR_SATURATION_MODIFIER = 0.85 -- 0.6
 NDefines.NGraphics.COUNTRY_COLOR_BRIGHTNESS_MODIFIER = 0.8 -- 0.8
 NDefines_Graphics.NMapMode.MAP_MODE_TERRAIN_TRANSPARENCY = 0.8
-NDefines_Graphics.NGraphics.BORDER_WIDTH = 1
+NDefines_Graphics.NGraphics.BORDER_WIDTH = 1.5
+NDefines_Graphics.NGraphics.PROVINCE_BORDER_FADE_NEAR = 200
+NDefines_Graphics.NGraphics.PROVINCE_BORDER_FADE_FAR = 300
+NDefines_Graphics.NGraphics.STATE_BORDER_FADE_NEAR = 400
+NDefines_Graphics.NGraphics.STATE_BORDER_FADE_FAR = 500
 NDefines_Graphics.NGraphics.CAMERA_ZOOM_SPEED_DISTANCE_MULT = 20
 NDefines_Graphics.NGraphics.MAPICON_GROUP_STRATEGIC_SIZE = 1000
 NDefines_Graphics.NGraphics.MAP_ICONS_STATE_HUGE = 100
